b7e7d6659d4e9c536befdc6e1a8df16cbd6080eb
Dane Natoli <dane.natoli@gmail.com>
2013-12-02 15:59:50 -0800

    Added data loading on document initialize.

--- spec/lib/pdf_tempura/document_spec.rb
+++ spec/lib/pdf_tempura/document_spec.rb
@@ -2,15 +2,63 @@ require 'spec_helper'
 
 describe PdfTempura::Document do
 
+  let(:dummy_class) do
+    Class.new(described_class)
+  end
+
+  describe "#new" do
+    let(:data) do
+      {
+        100 => {
+          first_name: "Stan",
+          surname: "Smith",
+          company: "CIA",
+          address: "The Pentagon, Washington, DC"
+        },
+        200 => {
+          emergency_contact: "Francine Smith",
+          phone_number: "123-456-7890"
+        }
+      }
+    end
+
+    before :each do
+      dummy_class.page(100){}
+      dummy_class.page(200){}
+    end
+
+    it "loads the data into each correct page" do
+      document = dummy_class.new(data)
+
+      document.pages.first.data.should == {
+        "first_name" => "Stan",
+        "surname" => "Smith",
+        "company" => "CIA",
+        "address" => "The Pentagon, Washington, DC"
+      }
+
+      document.pages.last.data.should == {
+        "emergency_contact" => "Francine Smith",
+        "phone_number" => "123-456-7890"
+      }
+    end
+
+    it "doesn't modify the class level pages" do
+      dummy_class.new(data)
+      dummy_class.pages.first.data.should == {}
+      dummy_class.pages.last.data.should == {}
+    end
+  end
+
   describe ".template" do
     it "sets the template file path" do
-      described_class.template "/some/path/to/template.pdf"
-      described_class.template_file_path.should == "/some/path/to/template.pdf"
+      dummy_class.template "/some/path/to/template.pdf"
+      dummy_class.template_file_path.should == "/some/path/to/template.pdf"
     end
 
     it "raises an argument error if you send it something that's not a string" do
       expect{
-        described_class.template [123]
+        dummy_class.template [123]
       }.to raise_error ArgumentError, "Template path must be a string."
     end
   end
@@ -20,52 +68,41 @@ describe PdfTempura::Document do
 
     it "yield a page object" do
       expect{ |block|
-        described_class.page(200, &block)
+        dummy_class.page(200, &block)
       }.to yield_with_args(page)
     end
 
     it "passed method calls on to the new page object" do
       PdfTempura::Page.any_instance.should_receive(:fields)
 
-      described_class.page(200) do
+      dummy_class.page(200) do
         fields
       end
     end
 
     it "adds the page object to the pages list" do
       expect{
-        described_class.page(200){}
-      }.to change(described_class.pages, :count).by(1)
+        dummy_class.page(200){}
+      }.to change(dummy_class.pages, :count).by(1)
     end
   end
 
   describe ".pages" do
     it "returns an array" do
-      described_class.pages.should be_a(Array)
+      dummy_class.pages.should be_a(Array)
     end
   end
 
   describe ".debug" do
     it "stores the debug options" do
-      described_class.debug :grid, :outlines
-      described_class.debug_options.should == [:grid, :outlines]
+      dummy_class.debug :grid, :outlines
+      dummy_class.debug_options.should == [:grid, :outlines]
     end
   end
 
   describe ".debug_options" do
     it "returns an array" do
-      described_class.pages.should be_a(Array)
-    end
-  end
-
-  let(:data){ {} }
-
-  subject{ described_class.new(data) }
-
-  describe "#data" do
-    let(:data){ { woo: "yes"} }
-    it "returns the data" do
-      described_class.new(data).data.should == data
+      dummy_class.pages.should be_a(Array)
     end
   end
 
